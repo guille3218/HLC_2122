1567ce5968720dc4cb2be545929c04fb716cfd1f
guille3218 <guillealminar2001@gmail.com>
2021-10-21 21:21:19 +0200

    Bucles

---
+++ Bucles/Ejercicio2.py
@@ -0,0 +1,9 @@
+x = ""
+
+i=1
+
+while i <=5:
+    x = x+str(i)+" "
+    print(x)
+    i += 1
+
---
+++ Bucles/Ejercicio3.py
@@ -0,0 +1,11 @@
+i = input("Introduce un número:")
+
+x=0
+suma = 0
+
+while x <=int(i):
+    suma += x
+    x+=1
+
+
+print("La suma es:",suma)
